df6c805bfacaa7bab707d9a463d76b1825f75104
Denis Cazzulo <deniscazzulo@icloud.com>
2026-05-09 14:30:36 +0200

    seo e correzioni

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.SITE_URL || undefined,
   output: 'server',
   adapter: node({
     mode: 'standalone',
